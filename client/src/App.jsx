4e24e93a35f7dd63dd1e9125368bdbafc0bcb478
Sandesh <sandeshsaravanan456@gmail.com>
2024-10-07 19:51:07 +0530

    Loading state done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,34 @@
 import { Header } from "./components";
 import { Outlet } from "react-router-dom";
 import { data, ingredientData, recipeData, dispenserData } from "./data/data";
-import { useEffect } from "react";
-function App() {
-  // Save data to localStorage
-  useEffect(() => {
-    localStorage.setItem("machines", JSON.stringify(data));
-    localStorage.setItem("ingredients", JSON.stringify(ingredientData));
-    localStorage.setItem("recipes", JSON.stringify(recipeData));
-    localStorage.setItem("dispensers", JSON.stringify(dispenserData));
-  }, []);
+import { useEffect, useState } from "react";
 
+function App() {
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    const saveDataToLocalStorage = () => {
+      localStorage.setItem("machines", JSON.stringify(data));
+      localStorage.setItem("ingredients", JSON.stringify(ingredientData));
+      localStorage.setItem("recipes", JSON.stringify(recipeData));
+      localStorage.setItem("dispensers", JSON.stringify(dispenserData));
+      setLoading(false); 
+    };
+
+    saveDataToLocalStorage(); 
+  }, []);
 
   return (
     <>
       <Header />
       <main className="pt-16 min-h-screen bg-[#141414] custom-scrollbar">
-       {localStorage.getItem("machines") && <Outlet />}
+        {loading ? ( 
+          <div className="flex justify-center items-center h-full">
+            <p className="text-white">Loading...</p>
+          </div>
+        ) : (
+          <Outlet /> 
+        )}
       </main>
     </>
   );
